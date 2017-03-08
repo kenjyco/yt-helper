42e89848a9ef290678f827578669048a9696afa7
Ken <kenjyco@gmail.com>
2017-03-08 09:54:56 -0600

    Initial commit
    Copied from
    https://github.com/kenjyco/beu/blob/ac1d75bfd15703acfd3996c9f2a7f218b2c4f99c/beu/download.py

---
+++ setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='yt-helper',
+    version='0.1.0',
+    description='Light wrapper to youtube-dl',
+    author='Ken',
+    author_email='kenjyco@gmail.com',
+    license='MIT',
+    url='https://github.com/kenjyco/yt-helper',
+    download_url='https://github.com/kenjyco/yt-helper/tarball/v0.1.0',
+    packages=find_packages(),
+    install_requires=[
+        'youtube-dl',
+        'click',
+        'input-helper',
+    ],
+    include_package_data=True,
+    package_dir={'': '.'},
+    package_data={
+        '' : ['*.ini'],
+    },
+    entry_points={
+        'console_scripts': [
+            'yt-download=yt_helper.scripts.download:main',
+        ],
+    },
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Software Development :: Libraries',
+        'Intended Audience :: Developers',
+    ],
+    keywords = ['youtube-dl', 'youtube', 'download']
+)
---
+++ yt_helper/scripts/download.py
@@ -0,0 +1,176 @@
+from __future__ import unicode_literals
+import os
+import re
+import click
+import input_helper as ih
+from glob import glob
+import youtube_dl
+
+"""
+See:
+
+- https://github.com/rg3/youtube-dl/blob/master/README.md#embedding-youtube-dl
+- https://github.com/rg3/youtube-dl/issues/6584
+"""
+
+
+def delete_all_extra_files(path='.'):
+    for fname in glob(os.path.join(path, '*.f???.*')):
+        os.remove(fname)
+        print('Removed {}'.format(repr(fname)))
+    for fname in glob(os.path.join(path, '**/*.f???.*')):
+        os.remove(fname)
+        print('Removed {}'.format(repr(fname)))
+
+
+class MyLogger(object):
+    def debug(self, msg):
+        print('== DEBUG ==', msg)
+
+    def warning(self, msg):
+        print('== WARNING ==', msg)
+
+    def error(self, msg):
+        print('== ERROR ==', msg)
+
+
+def my_hook(d):
+    if d['status'] == 'finished':
+        print('Downloaded {} ({}) in {}'.format(
+            d.get('filename', 'unknown file'),
+            d.get('_total_bytes_str', 'unknown bytes'),
+            d.get('_elapsed_str', 'unknown time'),
+        ))
+
+
+def av_from_url(url, **kwargs):
+    """Download audio and/or video from a URL with `youtube-dl`
+
+    - playlist: if True, allow downloading entire playlist
+    - quiet: if True, don't print messages to stdout
+    - thumbnail: if True, download thumbnail image of video
+    - description: if True, download description of video to a file
+    - subtitles: if True, embed subtitles in downloaded video (if available)
+    - template: string representing generated filenames
+    - audio_only: if True, don't keep the video file if one is downloaded
+    - mp3: if True, convert downloaded audio to MP3 file
+    """
+    ydl_opts = {
+        'restrictfilenames': True,
+        'ignoreerrors': True,
+        'noplaylist': not kwargs.get('playlist', False),
+        'quiet': kwargs.get('quiet', True),
+        'writethumbnail': kwargs.get('thumbnail', False),
+        'writedescription': kwargs.get('description', False),
+        'writesubtitles': kwargs.get('subtitles', False),
+        'keepvideo': True,
+        # 'format': 'bestvideo[ext!=webm]+bestaudio[ext!=webm]/best[ext!=webm]',
+        'format': 'bestvideo+bestaudio/best',
+        'logger': MyLogger(),
+        'progress_hooks': [my_hook],
+    }
+    if 'template' in kwargs and kwargs['template']:
+        ydl_opts.update({'outtmpl': kwargs['template']})
+    if kwargs.get('audio_only', False):
+        ydl_opts.update({
+            'keepvideo': False,
+            # 'format': 'bestaudio[ext!=webm]/best[ext!=webm]',
+            'format': 'bestaudio/best',
+        })
+    if kwargs.get('mp3', True):
+        ydl_opts.update({
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+                'preferredquality': '320',
+            }],
+        })
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url, download=False)
+        formats = info.pop('formats', None)
+        ydl.download([url])
+
+    for key in (
+        'age_limit',
+        'annotations',
+        'automatic_captions',
+        'episode_number',
+        'format_id',
+        'format_note',
+        'http_headers',
+        'is_live',
+        'license',
+        'player_url',
+        'preference',
+        'protocol',
+        'requested_subtitles',
+        'requested_formats',
+        'season_number',
+        'series',
+        'start_time',
+        'vcodec',
+        'webpage_url',
+        'webpage_url_basename',
+    ):
+        info.pop(key, None)
+
+    return info
+
+
+@click.command()
+@click.option(
+    '--template', '-o', 'template', default='',
+    help='string representing generated filenames'
+)
+@click.option(
+    '--playlist', '-p', 'playlist', is_flag=True, default=False,
+    help='Allow downloading entire playlist'
+)
+@click.option(
+    '--quiet', '-q', 'quiet', is_flag=True, default=False,
+    help='Don\'t print messages to stdout'
+)
+@click.option(
+    '--thumbnail', '-t', 'thumbnail', is_flag=True, default=False,
+    help='Download thumbnail image of video'
+)
+@click.option(
+    '--description', '-d', 'description', is_flag=True, default=False,
+    help='Download description of video to a file'
+)
+@click.option(
+    '--subtitles', '-s', 'subtitles', is_flag=True, default=False,
+    help='Embed subtitles in the downloaded video'
+)
+@click.option(
+    '--audio-only', '-a', 'audio_only', is_flag=True, default=False,
+    help='Don\'t keep the video file if one was downloaded'
+)
+@click.option(
+    '--mp3', '-m', 'mp3', is_flag=True, default=False,
+    help='Convert downloaded audio to MP3 file'
+)
+@click.argument('args', nargs=-1)
+def main(**kwargs):
+    """Wrapper to 'av_from_url'
+
+    - args: urls or filenames containing urls
+    """
+    args = kwargs.pop('args')
+    urls = ih.get_all_urls(*args)
+    results = []
+    for url in urls:
+        results.append(av_from_url(url, **kwargs))
+    delete_all_extra_files()
+    #
+    #
+    # return results
+    from pprint import pprint
+    pprint(results)
+    #
+    #
+
+
+if __name__ == '__main__':
+    main()
