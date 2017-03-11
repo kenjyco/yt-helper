ee72e4b26d44a36fd1234c9de8e5085b82dc77d0
Ken <kenjyco@gmail.com>
2017-03-11 11:22:06 -0600

    Add README to setup.py and bump to v0.1.4

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages
 
 
+with open('README.rst', 'r') as fp:
+    long_description = fp.read()
+
 setup(
     name='yt-helper',
-    version='0.1.3',
+    version='0.1.4',
     description='Light wrapper to youtube-dl',
+    long_description=long_description,
     author='Ken',
     author_email='kenjyco@gmail.com',
     license='MIT',
     url='https://github.com/kenjyco/yt-helper',
-    download_url='https://github.com/kenjyco/yt-helper/tarball/v0.1.3',
+    download_url='https://github.com/kenjyco/yt-helper/tarball/v0.1.4',
     packages=find_packages(),
     install_requires=[
         'youtube-dl',
